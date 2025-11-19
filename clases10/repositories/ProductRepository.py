7be5f477c0b85eb5e68791c10069a68d8d372b77
Franco Morales Castro <franco.morales@outlook.com>
2025-11-19 19:49:34 -0300

    feat: repositorio de productos completo

--- clases10/repositories/ProductRepository.py
+++ clases10/repositories/ProductRepository.py
@@ -30,5 +30,37 @@ class ProductRepository:
         return None
     
     # TODO: ELiminar producto
+    def delete_product(self, product_id: int) -> None:
+        query = f'''
+            DELETE FROM products
+            WHERE id = '{product_id}'
+        '''
+        self.database_connection.execute_query(query)
+    
     # TODO: Modificar producto
+    def update_product(self, product: Product) -> None:
+        query = f'''
+            UPDATE products
+            SET name = '{product.name}', price = '{product.price}', quantity = '{product.quantity}'
+            WHERE id = '{product.id}'
+        '''
+        self.database_connection.execute_query(query)
+
     # TODO: Buscar productos por nombre
+    def find_products_by_name(self, name: str) -> list[Product]:
+        query = f'''
+            SELECT id, name, price, quantity
+            FROM products
+            WHERE name LIKE '%{name}%'
+        '''
+        products_data = self.database_connection.fetch_all(query)
+        products = []
+        for product_data in products_data:
+            product = Product()
+            product.id = product_data[0]
+            product.name = product_data[1]
+            product.price = product_data[2]
+            product.quantity = product_data[3]
+            products.append(product)
+            
+        return products
